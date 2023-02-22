176e31c889e9eac5db19bbb4bc89b8692d68a1fe
SuperMirey <98138368+SuperMirey@users.noreply.github.com>
2023-02-21 18:14:40 -0600

    collapse try

--- js/script.js
+++ js/script.js
@@ -315,3 +315,19 @@ function navbarScroll() {
       header.classList.remove("is-shrunk");
     }
   });
+
+
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight){
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    });
+  }
